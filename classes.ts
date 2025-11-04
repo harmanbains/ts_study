f32fa4ff617fa97d2559f4b8442c29fc43f9d782
Harman Bains <bains.hmn@gmail.com>
2025-11-04 14:20:51 -0800

    field inheritence

--- classes.ts
+++ classes.ts
@@ -1,4 +1,7 @@
 class Vehicle {
+  constructor(public color: string) {
+  }
+
   drive(): void {
     console.log('chugga chugga');
   }
@@ -8,12 +11,18 @@ class Vehicle {
   }
 }
 
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color)
+
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color)
+  }
   drive(): void {
     console.log('vrooom');
   }
 }
 
-const car = new Car();
+const car = new Car(4, 'blue');
 car.drive();
 car.honk();
